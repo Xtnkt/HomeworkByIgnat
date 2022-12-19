b246973933199a044961431dee0a701103c55446
Vladislav Karas <dncmorgan@yandex.ru>
2022-12-19 21:25:36 +0300

    HW 11

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,3 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <img src={"https://v.fastcdn.co/u/430e104e/57579327-0-Loaders-3.svg"} className={s.img}/>
